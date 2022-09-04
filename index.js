a25206e5fd8d0963d47f4ac3f500a0259824bc20
Astika Luintel <astild2020@gmail.com>
2022-09-04 15:01:40 -0400

    commit tree f6ae500da186699ac90beff736315253c29f83c4

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+const returnFirstTwoDriversOne = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+ const returnFirstTwoDrivers= function(returnFirstTwoDriversOne){
+     let newArr = [];
+     newArr.push(returnFirstTwoDriversOne[0])
+     newArr.push(returnFirstTwoDriversOne[1])
+   return newArr;
+ }
+
+ const returnLastTwoDrivers = function (returnFirstTwoDriversOne){
+     let newArr = [];
+     newArr.push(returnFirstTwoDriversOne[2])
+     newArr.push(returnFirstTwoDriversOne[3])
+     return newArr;
+ }
+
+// ( const selectingDrivers = function(){
+ 
+//     return nyArr;
+//  };
+// )
+
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// selectingDrivers.push(returnFirstTwoDrivers);
+// selectingDrivers.push(returnLastTwoDrivers);
+
+// const fareQuintupler = createFareMultiplier(5);
+
+const  createFareMultiplier = (int) => {
+  return function (base = 5){
+    return base * int;
+  } 
+  };
+
+
+
+const fareDoubler =  function (fare){
+  return createFareMultiplier(2)(fare) 
+};
+
+const fareTripler = function(trip){
+  return createFareMultiplier(3)(trip)
+
+};
+
+function selectDifferentDrivers(driversArray, fn) {
+  return fn(driversArray);
+}
